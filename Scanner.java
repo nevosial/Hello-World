baa31c38749cd3e629ea842c3cd98b59d84bf23b
Neville Mascarenhas <nevosial@gmail.com>
2017-07-05 16:13:44 +0530

    STDI/O example

---
+++ Scanner.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		
+		System.out.println(a);
+	}
+}
+
+//We are using the Scanner class to take input from stdin.
