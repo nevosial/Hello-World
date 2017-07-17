3f4d5b6fe04ed2c863cfaa96a36c0a36c5886bcc
Neville Mascarenhas <nevosial@gmail.com>
2017-07-17 23:56:12 +0530

    Using aggregation

---
+++ jAggregate.java
@@ -0,0 +1,22 @@
+/***
+Program concept -> Aggregation.
+
+***/
+
+class Address{
+	String house;
+	String city;
+	String state;
+	int pincode;
+	String country;
+	
+	Address(String house, String city, String state, int pincode, String country){
+		this.house = house;
+		this. city = city;
+		this state = state;
+		this.pincode = pincode;
+		this.country = country;
+		
+	}
+}
+
